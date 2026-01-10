e3506d70e4380f3e9f7cab160eef3bc3aab0fbe7
Magalie Chetrit <1594036+BluePraise@users.noreply.github.com>
2026-01-10 09:36:20 -0500

    feat: refactor single condoleance template by removing unused header and date sections

--- templates/single-condoleance.php
+++ templates/single-condoleance.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Single Condoleance Template
  *
@@ -24,18 +25,13 @@ while (have_posts()) :
     $photos = get_post_meta(get_the_ID(), 'condoleance_photos', true);
     $candles_data = get_post_meta(get_the_ID(), 'condoleance_candles_data', true);
     $candle_count = is_array($candles_data) ? ($candles_data['count'] ?? 0) : 0;
+?>
 
-    // Allow themes to override hero styling
-    $hero_enabled = apply_filters('condoleance_hero_enabled', true);
-    ?>
-
-    <?php if ($hero_enabled) : ?>
-        <div class="hero-banner condoleance-hero">
-            <div class="container">
-                <h1 class="banner-heading"><?php the_title(); ?></h1>
-            </div>
+    <div class="hero-banner condoleance-hero">
+        <div class="container">
+            <h1 class="banner-heading"><?php the_title(); ?></h1>
         </div>
-    <?php endif; ?>
+    </div>
 
     <div class="block-content">
         <div class="container-fluid">
@@ -44,38 +40,16 @@ while (have_posts()) :
                 <?php do_action('condoleance_before_content', get_the_ID()); ?>
 
                 <article id="post-<?php the_ID(); ?>" <?php post_class('condoleance-single'); ?>>
-
-                    <?php if (!$hero_enabled) : ?>
-                        <header class="condoleance-header">
-                            <h1 class="condoleance-title"><?php the_title(); ?></h1>
-
-                            <?php if ($birth_date || $death_date) : ?>
-                                <div class="condoleance-dates">
-                                    <?php if ($birth_date) : ?>
-                                        <span class="birth-date">
-                                            <span class="date-label"><?php esc_html_e('Geboortedatum:', 'condoleance-register'); ?></span>
-                                            <time><?php echo esc_html($birth_date); ?></time>
-                                        </span>
-                                    <?php endif; ?>
-
-                                    <?php if ($death_date) : ?>
-                                        <span class="death-date">
-                                            <span class="date-label"><?php esc_html_e('Overlijdensdatum:', 'condoleance-register'); ?></span>
-                                            <time><?php echo esc_html($death_date); ?></time>
-                                        </span>
-                                    <?php endif; ?>
-                                </div>
-                            <?php endif; ?>
-                        </header>
-                    <?php endif; ?>
-
+                    <div class="condoleance-content" style="margin-bottom: 40px">
+                        <?php the_content(); ?>
+                    </div>
                     <?php if (!empty($photos) && is_array($photos)) : ?>
                         <?php do_action('condoleance_before_gallery', get_the_ID()); ?>
                         <div class="condoleance-gallery">
                             <?php
                             foreach ($photos as $photo_id => $photo_url) :
                                 $caption = wp_get_attachment_caption($photo_id);
-                                ?>
+                            ?>
                                 <div class="condoleance-gallery__item">
                                     <a href="<?php echo esc_url($photo_url); ?>" data-lightbox="condoleance-gallery" data-title="<?php echo esc_attr($caption); ?>">
                                         <img src="<?php echo esc_url($photo_url); ?>" alt="<?php echo esc_attr($caption); ?>" class="condoleance-gallery__image">
@@ -89,21 +63,13 @@ while (have_posts()) :
                         <?php do_action('condoleance_after_gallery', get_the_ID()); ?>
                     <?php endif; ?>
 
-                    <div class="condoleance-content" style="margin-bottom: 40px">
-                        <?php the_content(); ?>
-                    </div>
-
                     <div class="condoleance-info">
-                        <div class="condoleance-info__meta">
-                            <h6 class="condoleance-info__heading"><?php esc_html_e('Informatie', 'condoleance-register'); ?></h6>
-                            <?php echo do_shortcode('[condoleance_meta]'); ?>
-                        </div>
 
                         <div class="condoleance-info__candle">
                             <?php
                             $candle_gif = apply_filters('condoleance_candle_gif', site_url('/wp-content/uploads/2020/02/kaarsje.gif'));
-                            if ($candle_gif && @getimagesize($candle_gif)) :
-                                ?>
+                                if ($candle_gif && @getimagesize($candle_gif)) :
+                            ?>
                                 <img src="<?php echo esc_url($candle_gif); ?>" alt="<?php esc_attr_e('Gif van een kaarsje', 'condoleance-register'); ?>" class="condoleance-info__candle-gif">
                             <?php else : ?>
                                 <div class="condoleance-info__candle-icon">🕯️</div>
@@ -140,7 +106,7 @@ while (have_posts()) :
         </div>
     </div>
 
-    <?php
+<?php
 endwhile;
 
 get_footer();
